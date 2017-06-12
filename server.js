29458b4ac6e9ad4e1aba0eaff0952d710aac3027
Isabel <12ivazquez@gmail.com>
2017-06-12 20:40:45 +0000

    [#2/Isabe] Database being set up/configured

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.route('/').get(function(req, res) {
-    res.sendFile(process.cwd() + '/index.html');
-});
+app.use(bodyParser.json());
+app.use(cors());
 
 /*app.get('/', function (req, res) {
-  res.send('Hello World!')
-})*/
+  res.sendFile(process.cwd() + '/index.html');
+});*/
+
+app.get('/:urlToShorten(*)', function (req, res) {
+  var urltoShorten = req.params.urlToShorten;
+  res.json({"urlToShorten" : urltoShorten})
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
